2d8f0b6c9bb0b0b68d54084d1f34326c166cfd16
Jelani Alexander <jelanialexander82@gmail.com>
2022-12-27 17:30:38 +0900

    Deleted delete button

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -23,16 +23,11 @@ const Modal = ({ setIsOpen }) => {
             <p>Track your new friends on Social Media</p>
             The Green tab <BiHappyBeaming style={{ color: "green" }} /> means
             that you <br></br>want to contact your new friend soon. <br></br>
-            2X<MdMouse style={{ color: "green" }}/> to add the green tab.
+            2X
+            <MdMouse style={{ color: "green" }} /> to add the green tab.
           </div>
           <div className={styles.modalActions}>
             <div className={styles.actionsContainer}>
-              {/* <button
-                className={styles.deleteBtn}
-                onClick={() => setIsOpen(false)}
-              >
-                Delete
-              </button> */}
               <button
                 className={styles.deleteBtn}
                 onClick={() => setIsOpen(false)}
