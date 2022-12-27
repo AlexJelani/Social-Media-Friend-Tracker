3041c60d514e5cf8b23bc37a488d6cf1c712220c
Jelani Alexander <jelanialexander82@gmail.com>
2022-12-27 17:29:17 +0900

    Added Modal text

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -4,7 +4,7 @@ import React from "react";
 import styles from "./Modal.module.css";
 import { RiCloseLine } from "react-icons/ri";
 import { BiHappyBeaming } from "react-icons/bi";
-
+import { MdMouse } from "react-icons/md";
 
 const Modal = ({ setIsOpen }) => {
   return (
@@ -13,15 +13,17 @@ const Modal = ({ setIsOpen }) => {
       <div className={styles.centered}>
         <div className={styles.modal}>
           <div className={styles.modalHeader}>
-            <h5 className={styles.heading}>Dialog</h5>
+            <h5 className={styles.heading}>Social Media Friend Tracker</h5>
           </div>
           <button className={styles.closeBtn} onClick={() => setIsOpen(false)}>
             <RiCloseLine style={{ marginBottom: "-3px" }} />
           </button>
           <div className={styles.modalContent}>
             Welcome to my Social Friend Tracker App.<br></br>
-            The Green tab <BiHappyBeaming style={{color:"green"}}/> means that you want to contact 
-            your new friend 
+            <p>Track your new friends on Social Media</p>
+            The Green tab <BiHappyBeaming style={{ color: "green" }} /> means
+            that you <br></br>want to contact your new friend soon. <br></br>
+            2X<MdMouse style={{ color: "green" }}/> to add the green tab.
           </div>
           <div className={styles.modalActions}>
             <div className={styles.actionsContainer}>
